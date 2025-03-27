758b87d3e2eb1cc074b4f18f6796fc3f188d234b
unknown <alison.matthes@gmail.com>
2025-03-26 21:03:50 -0400

    Calculator is functional for base functions :) <3

--- Clackulator.ps1
+++ Clackulator.ps1
@@ -1,1 +1,5 @@
-﻿
+﻿[double]$DistanceInput = Read-Host "How many miles did you complete?"
+[double]$MinuteInput = Read-Host "How many minutes did you walk?"
+
+$MilePerHourOutput = $DistanceInput / $MinuteInput * 60
+Write-Host $MilePerHourOutput
